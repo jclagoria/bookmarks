75a4269e11c48a1a22ffd149343984cbe1caae56
jclagoria <Shenkun1979>
2016-07-09 02:51:08 -0300

    commit for managed user password

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -131,7 +131,7 @@ LOGOUT_URL = reverse_lazy('logout')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'juankalagoria@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shenkun1980'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
